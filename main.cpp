ff9ad72fbb78b3685665369f729fccd5a943ebe0
1080heidi <heidideleon415@gmail.com>
2025-12-10 18:17:31 -0800

    read whole file into map

--- main.cpp
+++ main.cpp
@@ -8,18 +8,20 @@ using namespace std;
 int main () {
     ifstream in("210-final-1-FA25.txt");
 
-    if (in.is_open()) {
-        cout << "Could not open file.\n";
+    if (!in) {
+        cerr << "Could not open file" << endl;
         return 1;
     }
 
-    map<string, int> traffic;
+     map<string, int> traffic;
 
     string origin, dest;
     
-    if (in >> origin >> dest) {
-        cout << "First pair: " << origin << " " << dest << endl;
+    while (in >> origin >> dest) {
+        traffic[origin]++;
     }
 
+    cout << "Size of map: " << traffic.size() << endl;
+    
     return 0;
 }
